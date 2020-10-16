0a77d73d00bf0ef963ee86b05be85a3b508d42fd
Matteo Gentili <matteo.gentili@progettopa.it>
2020-10-16 11:24:09 +0200

    fix server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var AlexaAppServer = require( 'alexa-app-server' );
+var AlexaAppServer = require('alexa-app-server');
 
-var server = new AlexaAppServer( {
-	httpsEnabled: false,
-	port: process.env.PORT || 80
-} );
+AlexaAppServer.start({
+	server_root: __dirname,     // Path to root
+	public_html: "public_html", // Static content
+	app_dir: "apps",            // Location of alexa-app modules
+	app_root: "",        // Service root // empty in our case
+	port: 80                 // Port to use
+});
 
-server.start();
